ad2d8a3054969040f0e43462d3000500b44ea32d
Ujjwal Shukla <ujjwal7017@gmail.com>
2023-01-12 17:02:38 +0530

    assignment 3 uploaded

---
+++ Assignment3/ans3.js
@@ -0,0 +1,14 @@
+//Array in JS
+
+const arr2 = new Array("Saab", "Volvo", "BMW");
+console.log(arr2);
+
+//Obj in JS
+
+const obj1 = {
+    name: "pankaj",
+    branch: "cse",
+    course: "btech"
+  };
+  
+  console.log(obj1);
---
+++ Assignment3/ans5.js
@@ -0,0 +1,15 @@
+const arr5 = new Array();
+arr5.push(12);
+arr5.push(23);
+arr5.push(5);
+arr5.push(13);
+arr5.push(40);
+arr5.push(15);
+arr5.push(10);
+console.log(arr5);
+
+arr5.sort();
+console.log(arr5);
+
+arr5.pop();
+console.log(arr5);
